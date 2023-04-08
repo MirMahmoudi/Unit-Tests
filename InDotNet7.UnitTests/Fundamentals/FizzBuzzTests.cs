31145b9e083834cf8e35246d03793d8da3d82cc7
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-04-08 14:15:21 +0200

    FizzBuzz in .Net 7

---
+++ InDotNet7.UnitTests/Fundamentals/FizzBuzzTests.cs
@@ -0,0 +1,37 @@
+﻿using InDotNet7.Fundamentals;
+using NUnit.Framework;
+
+namespace InDotNet7.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("fizzBuzz").IgnoreCase);
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(9);
+            Assert.That(result, Is.EqualTo("fizz").IgnoreCase);
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(10);
+            Assert.That(result, Is.EqualTo("buzz").IgnoreCase);
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheActualNumber()
+        {
+            var result = FizzBuzz.GetOutput(14);
+            Assert.That(result, Is.EqualTo("14").IgnoreCase);
+        }
+    }
+}
