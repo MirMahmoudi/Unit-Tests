de520e9e7e4cc762c83960b97d5b0ced78311588
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-04-10 20:26:04 +0200

    DemeritPointsCalculatorTests

---
+++ InDotNet7.UnitTests/Fundamentals/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using InDotNet7.Fundamentals;
+using NUnit.Framework;
+
+namespace InDotNet7.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demeritPointsCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowOutOfRangeException(int speed)
+        {
+            Assert.That(() =>
+                _demeritPointsCalculator.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                _demeritPointsCalculator.CalculateDemeritPoints(speed));
+
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+                _demeritPointsCalculator.CalculateDemeritPoints(speed));
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(71, 1)]
+        [TestCase(115, 10)]
+        public void CalculateDemeritPoints_SpeedIsIsInRange_ReturnDemeritPoints(int speed, int demeritPoints)
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(demeritPoints));
+        }
+    }
+}
--- InDotNet7/Fundamentals/DemeritPointsCalculator.cs
+++ InDotNet7/Fundamentals/DemeritPointsCalculator.cs
@@ -3,14 +3,19 @@
     public class DemeritPointsCalculator
     {
         private const int SpeedLimit = 65;
-        
+        private const int MaxSpeed = 300;
+
         public int CalculateDemeritPoints(int speed)
         {
-            if (speed < 0) 
-                throw new ArgumentOutOfRangeException();
-            
-            if (speed <= SpeedLimit) return 0; 
-            
+            switch (speed)
+            {
+                case < 0:
+                case > MaxSpeed:
+                    throw new ArgumentOutOfRangeException();
+                case <= SpeedLimit:
+                    return 0;
+            }
+
             const int kmPerDemeritPoint = 5;
             var demeritPoints = (speed - SpeedLimit)/kmPerDemeritPoint;
 
