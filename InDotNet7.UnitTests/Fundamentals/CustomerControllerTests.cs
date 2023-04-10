4a9948a6f190150162c468b360084ba7abd70449
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-04-10 20:19:24 +0200

    CustomerControllerTests

---
+++ InDotNet7.UnitTests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using InDotNet7.Fundamentals;
+using NUnit.Framework;
+
+namespace InDotNet7.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var result = _customerController.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+            Assert.That(result, Is.TypeOf(typeof(NotFound)));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void GetCustomer_IdIsNotZero_ReturnOk(int id)
+        {
+            var result = _customerController.GetCustomer(id);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+            Assert.That(result, Is.TypeOf(typeof(Ok)));
+        }
+    }
+}
---
+++ InDotNetFramework48.UnitTests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using InDotNetFramework48.Fundamentals;
+using NUnit.Framework;
+
+namespace InDotNetFramework48.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var result = _customerController.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+            Assert.That(result, Is.TypeOf(typeof(NotFound)));
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var result = _customerController.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+            Assert.That(result, Is.TypeOf(typeof(Ok)));
+        }
+    }
+}
