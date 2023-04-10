8d9bace230526a9e12fff0f0e724acda64a487b9
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-04-10 20:20:43 +0200

    HtmlFormatterTests

---
+++ InDotNetFramework48.UnitTests/Fundamentals/HtmlFormatterTests.cs
@@ -0,0 +1,21 @@
+﻿using InDotNetFramework48.Fundamentals;
+using NUnit.Framework;
+
+namespace InDotNetFramework48.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var htmlFormatter = new HtmlFormatter();
+            var result = htmlFormatter.FormatAsBold("abc");
+
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+            Assert.That(result, Does.Contain("abc"));
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+        }
+    }
+}
